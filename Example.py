34ea6024387e82b368b145e83b13975b5346537a
Harsha Veena Tadavarthy <159509287+dhvl1754@users.noreply.github.com>
2024-05-07 01:15:09 -0400

    Update Example.py

--- Example.py
+++ Example.py
@@ -1,33 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
+#Installation
+git clone https://github.com/dhvl1754/Phonet_English'
 
-# In[19]:
-
-
-get_ipython().system(' git clone https://github.com/dhvl1754/Phonet_English')
-
-
-# In[13]:
-
-
+#Importing library
 from Phonet_English.Phonet_English import Phonet_English
 
-
-# In[14]:
-
-
 phon_Eng=Phonet_English(["all"])
-
-
-# In[18]:
-
-
 file_audio="sentence.wav"
 df = phon_Eng.get_PLLR(file_audio)
-df
+print(df)
 
-
-# In[ ]:
 
 
 
